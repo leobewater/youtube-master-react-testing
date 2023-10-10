f4e5808d45d896a6eecde5706d8ef4d60d98f071
leobewater <mmbee888@gmail.com>
2023-10-10 12:08:20 -0400

    add userform

---
+++ 01-users/src/components/UserForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const UserForm = () => {
+  return (
+    <form>
+      <div>
+        <label>Name</label>
+        <input />
+      </div>
+      <div>
+        <label>Email</label>
+        <input />
+      </div>
+      <button>Add User</button>
+    </form>
+  );
+};
+
+export default UserForm;

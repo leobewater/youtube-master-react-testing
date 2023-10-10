5d183c6066b69f7665ce685e14d6067f8f3a42b2
leobewater <mmbee888@gmail.com>
2023-10-10 12:12:07 -0400

    set name and email states

--- 01-users/src/components/UserForm.js
+++ 01-users/src/components/UserForm.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UserForm = () => {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(name, email);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label>Name</label>
-        <input />
+        <input value={name} onChange={(e) => setName(e.target.value)} />
       </div>
       <div>
         <label>Email</label>
-        <input />
+        <input value={email} onChange={(e) => setEmail(e.target.value)} />
       </div>
       <button>Add User</button>
     </form>

1e74a195e0651880a8ad882671ee4c827218a193
leobewater <mmbee888@gmail.com>
2023-10-10 15:01:41 -0400

    test table cell display text

--- 01-users/src/components/UserList.test.js
+++ 01-users/src/components/UserList.test.js
@@ -8,17 +8,35 @@ test('render one row per user', () => {
     { name: 'sam', email: 'sam@sam.com' },
   ];
 
+  // const { container } = render(<UserList users={users} />);
   render(<UserList users={users} />);
 
   // browser tool
-  // screen.logTestingPlaygroundURL();
+  //  screen.logTestingPlaygroundURL();
 
   // const rows = screen.getAllByRole('row');
   const rows = within(screen.getByTestId('users')).getAllByRole('row');
-  
+
+  // show warning, just ignore it by adding the following line
+  // eslint-disable-next-line
+  // const rows = container.querySelectorAll('tbody tr');
+
   expect(rows).toHaveLength(2);
 });
 
 test('render the email and name of each user', () => {
-  render(<UserList users={[]} />);
+  const users = [
+    { name: 'jane', email: 'jane@jane.com' },
+    { name: 'sam', email: 'sam@sam.com' },
+  ];
+
+  render(<UserList users={users} />);
+
+  for (let user of users) {
+    const name = screen.getByRole('cell', { name: user.name });
+    const email = screen.getByRole('cell', { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
 });
